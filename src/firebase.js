815e8e7807d666e9b13d2aa0d5fb6759f4fdf5a9
NaveedAshraf1 <example@example.com>
2025-08-19 22:15:57 +0500

    chore(firebase): switch to new Firebase project (jenom-c8a69) and add databaseURL

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,14 @@ import { getAnalytics } from "firebase/analytics";
 import { initializeFirestore, persistentLocalCache, persistentMultipleTabManager } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAXxTQ_8A7ECulaz8ukxJOBiRqrxYEJ73M",
-  authDomain: "axis-21017.firebaseapp.com",
-  projectId: "axis-21017",
-  storageBucket: "axis-21017.firebasestorage.app",
-  messagingSenderId: "563797511582",
-  appId: "1:563797511582:web:07ea613aade8aec9369a8f",
-  measurementId: "G-KCTD8FE7YZ"
+  apiKey: "AIzaSyDQNWyYplMHDAJu1bhfHG0Cr1UWQTrB9Iw",
+  authDomain: "jenom-c8a69.firebaseapp.com",
+  databaseURL: "https://jenom-c8a69-default-rtdb.firebaseio.com",
+  projectId: "jenom-c8a69",
+  storageBucket: "jenom-c8a69.firebasestorage.app",
+  messagingSenderId: "1076986591746",
+  appId: "1:1076986591746:web:8c381638ccc0eddd15c51b",
+  measurementId: "G-FS8HEKF6NQ"
 };
 
 console.log('[FIREBASE] Initializing Firebase with project:', firebaseConfig.projectId, 'appId:', firebaseConfig.appId);
